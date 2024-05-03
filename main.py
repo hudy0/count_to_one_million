abd36b4bdb35c7c314884e0e8f76ae1151c3bd55
hudaifa <hudaifa890@gmail.com>
2024-05-03 23:58:14 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,46 +1,5 @@
-import threading
-import time
-
-
-def count_chunk(start, end):
-    count = 0
-    for i in range(start, end):
-        count += 1
-    return count
-
-
-def count_to_one_million_parallel():
-    start_time = time.time()
-    num_threads = 4  # Adjust as needed
-    chunk_size = 1000000 // num_threads
-    threads = []
-    results = []
-
-    def thread_worker(start, end):
-        result = count_chunk(start, end)
-        results.append(result)
-
-    for i in range(num_threads):
-        start = i * chunk_size + 1
-        end = (i + 1) * chunk_size + 1
-        thread = threading.Thread(target=thread_worker, args=(start, end))
-        thread.start()
-        threads.append(thread)
-
-    for thread in threads:
-        thread.join()
-
-    count = sum(results)
-    end_time = time.time()
-    return end_time - start_time, count
-
-
-if __name__ == "__main__":
-    time_taken, count = count_to_one_million_parallel()
-    print("Time taken to count to 1 million in parallel (with multithreading):", time_taken, "seconds")
-    print("Total count:", count)
-
-"""import multiprocessing
+"""
+import multiprocessing
 import time
 
 
